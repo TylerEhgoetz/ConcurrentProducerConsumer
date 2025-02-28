06df79caad4fe3b6f65b6fb64a43eb0dafaa0fca
Tyler Ehgoetz <tyler.ehgoetz@gmail.com>
2025-02-27 19:04:46 -0500

    initial simple setup for a threadsafe lock using queue

---
+++ include/ThreadSafeQueue.h
@@ -0,0 +1,34 @@
+#ifndef THREADSAFEQUEUE_H
+#define THREADSAFEQUEUE_H
+
+#include <condition_variable>
+#include <mutex>
+#include <queue>
+
+template <typename T>
+class ThreadSafeQueue
+{
+public:
+    void push(const T& value)
+    {
+        std::lock_guard<std::mutex> lock(m_mutex);
+        m_queue.push(value);
+        m_conditionVariable.notify_one();
+    }
+
+    T pop()
+    {
+        std::unique_lock<std::mutex> lock(m_mutex);
+        m_conditionVariable.wait(lock, [this] { return !m_queue.empty(); });
+        T value = m_queue.front();
+        m_queue.pop();
+        return value;
+    }
+
+private:
+    std::queue<T>           m_queue;
+    mutable std::mutex      m_mutex;
+    std::condition_variable m_conditionVariable;
+};
+
+#endif   // THREADSAFEQUEUE_H
